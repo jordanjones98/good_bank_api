e1f0db4799e52446e6b019dd1cf305060d685c94
Jordan Jones <me@jjones.tech>
2023-08-07 20:45:38 -0500

    bad boy alert

---
+++ config.local.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyC8XA0r7k8hzY6qo-PbQouFe0-Frz4gWLo",
+  authDomain: "goodbank-7b481.firebaseapp.com",
+  projectId: "goodbank-7b481",
+  storageBucket: "goodbank-7b481.appspot.com",
+  messagingSenderId: "237291422272",
+  appId: "1:237291422272:web:6619465ec8c9a7eef895a5",
+};
